9ae16e8bccfb3d11ff131cb1e21e23f081525c3d
anagha97 <anagharaghunathk@gmail.com>
2025-02-10 10:08:32 +0100

    added sample cpp file

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+// my_cpp_library/example.cpp
+#include <iostream>
+
+void unusedFunction() {
+    std::cout << "This function is never used!" << std::endl;
+}
+
+int main() {
+    std::cout << "Hello, C++!" << std::endl;
+    return 0;
+}
